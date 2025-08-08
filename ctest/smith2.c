c2995fe3f4f784420a22673fb3fd70e006ad53de
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-08-08 18:48:30 +0800

    archive

---
+++ ctest/smith2.c
@@ -0,0 +1,28 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.3.0
+ * Git version: 30dccd73b
+ * Options:   (none)
+ * Seed:      12412652534697954946
+ */
+
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   uint64_t  f1;
+   int8_t  f2;
+   int32_t  f3;
+};
+
