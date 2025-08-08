b793b96d60d8bec2c5c247ae03685e2e64ff1af0
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-08-08 17:55:33 +0800

    add stop-after-lex

--- ctest/smith.c
+++ ctest/smith.c
@@ -7,6 +7,15 @@
  * Seed:      12412652534697954946
  */
 
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
 static long __undefined;
 
 /* --- Struct/Union Declarations --- */
