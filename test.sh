cb1e72606ad1292e7b75fe4beab31c730c091765
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-08-28 10:11:36 +0800

    add unary op

--- test.sh
+++ test.sh
@@ -2,25 +2,25 @@
 
 # 运行 moon check
 echo "Running moon check..."
-if ! moon check --warn-list -A; then
+if ! moon check --warn-list -1-2-6-28; then
     echo "Test Failed"
     exit 1
 fi
 
 # 运行 moon test
 echo "Running moon test..."
-if ! moon test --warn-list -A; then
+if ! moon test --warn-list -1-2-6-28; then
     echo "Test Failed"
     exit 1
 fi
 
 # 定义文件数组
-files=("ret42.c" "simple_add.c" "simple_addf.c")
+files=("ret42.c" "simple_add.c" "simple_addf.c" "simple_unary.c")
 
 # 循环处理每个文件
 for filename in "${files[@]}"; do
     echo "Testing file: $filename"
-    if ! moon run main --warn-list -A -- -file "ctest/$filename" --no-print-llvm; then
+    if ! moon run main --warn-list -1-2-6-28 -- -file "ctest/$filename" --no-print-llvm; then
         echo "Test Failed"
         exit 1
     fi
