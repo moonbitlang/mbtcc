94cf880956f8d03c2ec4ba3b321196078979d095
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-08-28 17:15:17 +0800

    add struct2.c

---
+++ ctest/struct2.c
@@ -0,0 +1,21 @@
+void println_int(int x);
+
+struct Point {
+  int x;
+  int y;
+};
+
+int sum_point_coords(struct Point *p) {
+  return p->x + p->y;
+}
+
+int main() {
+  struct Point p1;
+  p1.x = 3;
+  p1.y = 4;
+
+  int z = sum_point_coords(&p1);
+  println_int(z);
+  return 0;
+}
+
