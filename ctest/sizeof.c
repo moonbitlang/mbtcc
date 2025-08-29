d63ab2803ef0634b02f52d1c56016fb1313a102e
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-08-29 15:48:28 +0800

    make sizeof passed

--- ctest/sizeof.c
+++ ctest/sizeof.c
@@ -1,14 +1,45 @@
 
 void println_int(int);
 
-typedef struct {
-  int a;
-  char b;
-} S;
+typedef struct { int a; char b; } S1;
+typedef struct { int a; char b; char c; } S2;
+typedef struct { int a; char b; char c; char d; } S3;
+typedef struct { int a; char b; char c; char d; char e; } S4;
+typedef struct { int a; double b; } S5;
+typedef struct { double a; int b; } S6;
+typedef struct { S1 a; int b; } S7;
+
+
+
+int is_eq(int a, int b) {
+  if (a == b) {
+    return 1;
+  }
+  return 0;
+}
 
 int main() {
-  int a = sizeof(S);
+  int passed = 0;
+  passed += is_eq(1, sizeof(char));
+  passed += is_eq(2, sizeof(short));
+  passed += is_eq(4, sizeof(int));
+  passed += is_eq(4, sizeof(unsigned));
+  passed += is_eq(4, sizeof(unsigned int));
+  passed += is_eq(8, sizeof(long));
+  passed += is_eq(8, sizeof(long long));
+  passed += is_eq(8, sizeof(unsigned long));
+  passed += is_eq(8, sizeof(unsigned long long));
+  passed += is_eq(4, sizeof(float));
+  passed += is_eq(8, sizeof(double));
+  passed += is_eq(8, sizeof(void*));
+  passed += is_eq(8, sizeof(S1));
+  passed += is_eq(8, sizeof(S2));
+  passed += is_eq(8, sizeof(S3));
+  passed += is_eq(8, sizeof(S4));
+  passed += is_eq(16, sizeof(S5));
+  passed += is_eq(16, sizeof(S6));
+  passed += is_eq(16, sizeof(S7));
 
-  println_int(a);
+  println_int(passed);
   return 0;
 }
