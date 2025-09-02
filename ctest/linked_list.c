1e4a2435ab9dad1c3ac4090da4ec6d235396b3c6
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-09-02 16:04:56 +0800

    add preprocess

--- ctest/linked_list.c
+++ ctest/linked_list.c
@@ -1,6 +1,8 @@
 
 #include "test.h"
 
+#define NULL 0
+
 typedef struct Node {
   int data;
   struct Node* next;
@@ -9,18 +11,18 @@ typedef struct Node {
 Node* create_node(int data) {
   Node* new_node = (Node*)malloc(sizeof(Node));
   new_node->data = data;
-  new_node->next = 0;
+  new_node->next = NULL;
   return new_node;
 }
 
 void append(Node** head_ref, int new_data) {
   Node* new_node = create_node(new_data);
-  if (*head_ref == 0) {
+  if (*head_ref == NULL) {
     *head_ref = new_node;
     return;
   }
   Node* last = *head_ref;
-  while (last->next != 0) {
+  while (last->next != NULL) {
     last = last->next;
   }
   last->next = new_node;
@@ -29,25 +31,25 @@ void append(Node** head_ref, int new_data) {
 void delete_list(Node** head_ref) {
   Node* current = *head_ref;
   Node* next;
-  while (current != 0) {
+  while (current != NULL) {
     next = current->next;
     free(current);
     current = next;
   }
-  *head_ref = 0;
+  *head_ref = NULL;
 }
 
 void sum_list(Node* head, int* sum) {
-  *sum = 0;
+  *sum = NULL;
   Node* current = head;
-  while (current != 0) {
+  while (current != NULL) {
     *sum += current->data;
     current = current->next;
   }
 }
 
 int main() {
-  Node* head = 0;
+  Node* head = NULL;
   append(&head, 1);
   append(&head, 2);
   append(&head, 3);
